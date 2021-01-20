315bf12e3916449660bd914c0600f97ef2cf1bb7
James Atkinson <CSAtkinson8588@gmail.com>
2021-01-20 16:51:56 -0500

    Changed from class based states to hooks and functional based states

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-
-  state = {
+const App = props => {  
+  const [personsState, setPersonsState] = useState({
     persons: [
       {name: 'James', age: 32},
       {name: 'Anslee', age: 18},
       {name: 'Dawson', age: 21}
-    ]
-  }
+    ],
+    otherState: 'Some other value'
+  });
 
-  switchNameHandler = () => {
-    this.setState({
+  const switchNameHandler = () => {
+    setPersonsState({
       persons: [
         {name: 'Bornforexile', age: 32},
         {name: 'Anslee', age: 25},
@@ -22,20 +22,23 @@ class App extends Component {
     })
   }
 
-  render() {
     return (
       <div className="App">
        <h1>Hi, I'm a react app!</h1>
        <p>This is really working!</p>
-       <button onClick={this.switchNameHandler}>Switch Name</button>
-       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-       <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>I am a freckled Firecracker</Person>
-       <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+       <button onClick={switchNameHandler}>Switch Name</button>
+       <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+       <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>I am a freckled Firecracker</Person>
+       <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
       </div>
     );
 
     //return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'Does this work now?'))
   }
-}
+
 
 export default App;
+
+
+
+
